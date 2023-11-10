3d310e5ac77084283579675e632c1ca6d55893b5
Guilherme Silva <guilherme92325@gmail.com>
2023-11-10 15:54:37 -0300

    refactor: corrected class and folder name

--- api/src/main/java/br/com/ibico/api/constants/SMSConstants.java
+++ api/src/main/java/br/com/ibico/api/constants/SMSConstants.java
@@ -1,9 +1,9 @@
-package br.com.ibico.api.constraints;
+package br.com.ibico.api.constants;
 
-public class SmsConstraints {
+public class SMSConstants {
 
     public static final String SMS_PASSWORD_RESET_MESSAGE = "Oi, %s. Aqui é o IBico. Para redefinir sua senha, use o código %s.";
 
-    private SmsConstraints() {
+    private SMSConstants() {
     }
 }
---
+++ api/src/main/java/br/com/ibico/api/constants/SecurityConstants.java
@@ -0,0 +1,9 @@
+package br.com.ibico.api.constants;
+
+public class SecurityConstants {
+    public static final String JWT_KEY = "v+BgNsSDTOIdIRffpz2MYcYZu3Lun160JGk0wkkD1OYFWsUfwFtS2DzmR4pGAvaT";
+
+    public static final Long JWT_EXPIRATION = 60L;
+    private SecurityConstants() {
+    }
+}
