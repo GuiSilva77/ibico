37270f30b65c6def3124e9d9fbaa2dd14a0a8393
Guilherme Silva <guilherme92325@gmail.com>
2023-11-11 20:07:56 -0300

    feat: preparation for AWS deploy

--- api/src/main/java/br/com/ibico/api/security/SecurityConfig.java
+++ api/src/main/java/br/com/ibico/api/security/SecurityConfig.java
@@ -1,26 +1,27 @@
 package br.com.ibico.api.security;
 
-import br.com.ibico.api.filters.CsrfCookieFilter;
 import br.com.ibico.api.filters.JWTTokenGeneratorFilter;
 import br.com.ibico.api.filters.JwtValidatorFilter;
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Collections;
+import java.util.Arrays;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     private final RequestMatcher permitRequests;
@@ -31,18 +32,13 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        CsrfTokenRequestAttributeHandler csrfTokenRequestAttributeHandler = new CsrfTokenRequestAttributeHandler();
-        csrfTokenRequestAttributeHandler.setCsrfRequestAttributeName("_csrf");
 
         http.sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .csrf(csrf -> csrf
-                        .ignoringRequestMatchers("/**")
-                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
-                .cors(cors -> cors.configurationSource(this::getCorsConfiguration))
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(getCorsConfiguration()))
                 .addFilterBefore(new JwtValidatorFilter(permitRequests), BasicAuthenticationFilter.class)
                 .addFilterAfter(new JWTTokenGeneratorFilter(), BasicAuthenticationFilter.class)
-                .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
                 .authorizeHttpRequests(request -> request
                         .requestMatchers(permitRequests).permitAll()
                         .anyRequest().authenticated())
@@ -57,14 +53,16 @@ public class SecurityConfig {
     }
 
     @Bean
-    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+    public CorsConfigurationSource getCorsConfiguration() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(Collections.singletonList("*"));
-        config.setAllowedMethods(Collections.singletonList("*"));
-
-        //config.setAllowCredentials(true);
-        //config.setAllowedHeaders(Collections.singletonList("Authorization"));
+        config.setAllowedOrigins(Arrays.asList("http://localhost:8080", "https://ibico.vercel.app/"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        config.setAllowCredentials(true);
+        config.setAllowedHeaders(Arrays.asList("Authorization", "Requestor-Type"));
+        config.setExposedHeaders(Arrays.asList("X-Get-Header"));
         config.setMaxAge(3600L);
-        return config;
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 }
